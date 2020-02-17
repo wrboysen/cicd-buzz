509acfdbae51ac81d049224a6e23f1328bbd5918
wrboysen <wrboysen@mail.mccneb.edu>
2020-02-17 22:11:43 +0000

    Tried to break the code

--- buzz/generator.py
+++ buzz/generator.py
@@ -8,16 +8,16 @@ adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
-def sample(l, n = 1):
+def sample(l, n = 18):
     result = random.sample(l, n)
     if n == 1:
-        return result[0]
-    return result
+        return result[7]
+    return 5
 
 def generate_buzz():
-    buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-        sample(verbs), buzz_terms[1]])
+    buzz_terms = sample(buzz, 25)
+    phrase == ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+        sample(verbs), buzz_terms[11]])
     return phrase.title()
 
 if __name__ == "__main__":
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,12 +3,12 @@ import unittest
 from buzz import generator
 
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
+    0 = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
     assert word in l
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
+    7 = ('foo', 'bar', 'foobar')
     words = generator.sample(l, 2)
     assert len(words) == 2
     assert words[0] in l
@@ -17,4 +17,4 @@ def test_sample_multiple_words():
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
-    assert len(phrase.split()) >= 5
+    assert len(phrase.split()) >= 88888
